bc2d9af452b76214e345eee16dbc5598193595ff
WoolfStore <45537072+WoolfStore@users.noreply.github.com>
2018-12-25 23:34:31 +0300

    Delete V502.cpp

--- V502.cpp
+++
@@ -1,6 +0,0 @@
-
-void changeEnemyPosition(int x, int y); // Вызывать каждый раз при движении врага. (Принимает на борт координаты врага)
-void changeEnemyBulletPosition(int x, int y); // При движении вражеской пули. (Координаты пули)
-void fightWithAShadow(int width, int height); // Вызывать всегда (передавть ширину монстра, который сейчас на экране)
-bool niceShoot(int width, int height); //Вызывать всегда. Возвращает true при попадании во врага. Принимает ширину врага.
-
